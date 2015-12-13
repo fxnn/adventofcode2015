12f15e374c0f49dd1fa9de281a42a807a77e9b5e
Felix Neumann <dev@fxnn.de>
2015-12-13 23:25:25 +0100

    Day 1, second half

--- solution1.go
+++ solution1.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	var input = read(inputFilePath())
 	var floor = 0
+	var firstBasementPosition = 0
+
 	for i := range input {
 		switch input[i] {
 		case '(':
@@ -18,8 +20,14 @@ func main() {
 		default:
 			log.Printf("unknown input %s", input[i])
 		}
+
+		if floor == -1 && firstBasementPosition == 0 {
+			firstBasementPosition = i + 1
+		}
 	}
+
 	log.Printf("santa is at floor %d", floor)
+	log.Printf("he entered the basement first at position %d", firstBasementPosition)
 }
 
 func read(path string) (result []byte) {
